66f1085a49ce8588b8b130849b18d4c21164ccf2
Eric Booker <ericbooker12@gmail.com>
2016-08-15 11:12:58 -0700

    Add ajax request for wells view

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 
 	// $('#graphic_container').rotate(90);
+	rotateElement();
+	enbigify();
+	showWells();
+
 
   	$("#show_table").hide();
 
@@ -19,45 +23,62 @@ $(document).ready(function() {
 	    $("#show_table").hide();
 	    $("#show_graph").show();
 	  });
+});
 
+//--------------------Functions---------------------
 
+var enbigify = function(){
 	$('div .pic').on('mousedown', function() {
 		$(this).height($(this).height()* 2);
 		$(this).width($(this).width()* 2);
-
-
 		console.log('pic clicked');
 	});
 
 	$('div .pic').on('mouseup', function() {
 		$(this).height($(this).height()/ 2);
 		$(this).width($(this).width()/ 2);
-
-		
 		console.log('pic clicked');
 	});
+};
 
-
-
+var rotateElement = function() {
 	$('div .rotate').on('click', function(){
-
-	$(this).rotate({bind:{
-  		click: function(){
+		$(this).rotate({bind:{click: function(){
 	    	$(this).rotate({
 	      		duration:2000,
 	      		angle: 0,
 	     		animateTo:360
       		})
-    	}
-	}})
+	    }}})
+	});	
+};
+
+var showWells = function(){
+	$('#header_title #wells').on('click', function(event) {
+		event.preventDefault();
+		console.log('Wells link clicked');
+
+		var urlVariable = $(this).attr('href');
+		var method = 'GET';
+
+		var request = $.ajax({
+			url: urlVariable,
+			type: method
+		});
+
+		request.done(function(responseData){
+			console.log('Request successful');
+			$('#main_index').html(responseData);
+		});
+
+		request.fail(function(responseData){
+			alert('ajax request failed');
+		});
 
 
-});
 
-
-
-
-});
+	});
+};
 
 
 
