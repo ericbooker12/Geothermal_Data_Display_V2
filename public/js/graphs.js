7e7f5250490b2096d1d4636259f1fa8a5038528d
Eric Booker <ericbooker12@gmail.com>
2016-08-18 19:57:25 -0700

    Hide columns until charts are completed

--- public/js/graphs.js
+++ public/js/graphs.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
   console.log("ready from graphs.js");
+  $('#col1').hide(); 
+  $('#col2').hide(); 
+  $('#col3').hide(); 
+  $('#col4').hide(); 
 
   getFields();
 
@@ -7,9 +11,10 @@ $(document).ready(function() {
 
 });
 
+
     WIDTH = 400,
     HEIGHT = 2000,
-    MARGINS = { top: 20, right: 20, bottom: 20, left: 50 };
+    MARGINS = { top: 20, right: 20, bottom: 20, left: 0 };
 
 //----------------------------------------------------
 // Secondary method to get data from db. 
@@ -18,12 +23,11 @@ var getFields = function() {
   console.log("Inside getFields function")
 
   // $('#show_chart').on('click', function(event){
-    event.preventDefault();
-    console.log("'Show Chart' clicked")
+  //   event.preventDefault();
+  //   console.log("'Show Chart' clicked")
   
     var urlVariable = '/measurements';
     var method = 'GET';
-    var wellDataJson = [];
 
     var request = $.ajax({
       url: urlVariable,
@@ -43,17 +47,18 @@ var getFields = function() {
   // });
 }; 
 
-//----------------------------------------------------
+//--------------------------------------------------------
+
 // call chart functions
 var callCharts = function(responseData) {
   console.log("Inside callCharts()")
-  console.log("Call createNvd3Chart()")
   var data = JSON.parse(responseData);
 
-  createNvd3Chart(data, '#col1', "rop", "Rate of Penetration");
-  createNvd3Chart(data, '#col2', "tempOut", "Temperature Out degF");
-  createNvd3Chart(data, '#col3', "pressure", "Pressure psi");
-  createNvd3Chart(data, '#col4', "wob", "Weight on Bit k-lb");
+  console.log("Call createNvd3Chart()")
+  createNvd3Chart(data, '#col1', "rop", "Rate of Penetration", 200);
+  createNvd3Chart(data, '#col2', "tempOut", "Temperature Out degF", 300);
+  createNvd3Chart(data, '#col3', "pressure", "Pressure psi", 1200);
+  createNvd3Chart(data, '#col4', "wob", "Weight on Bit k-lb", 100);
 
 };
 
@@ -61,10 +66,12 @@ var callCharts = function(responseData) {
 
 // Create vertical graph using nvd3.js
 // later: combine param and label into array or object.
-var createNvd3Chart = function(data, selector, param, label) {
+var createNvd3Chart = function(data, selector, param, label, chartRange) {
   nv.addGraph(function() {
     
   var chartData = formatData(data, param, label); 
+
+  console.log(chartData);
     
   // Get the last depth, y-value in the data.
   var finalDepth = chartData[0].values.slice(-1)[0].y; 
@@ -76,19 +83,21 @@ var createNvd3Chart = function(data, selector, param, label) {
     .showYAxis(true)          //Show the y-axis
     .showXAxis(true)          //Show the x-axis
     .yDomain([finalDepth, 0])
-    .xDomain([0, 400]);
+    .xDomain([0, chartRange]);
 
   $(selector).height(HEIGHT).width(WIDTH);
 
   chart.xAxis     //Chart x-axis settings
-    .axisLabel('Temp')
+    .axisLabel(label)
     .tickFormat(d3.format(4));
 
   chart.yAxis     //Chart y-axis settings
-    .axisLabel('depth')
+    .axisLabel('Depth (ft)')
     .tickFormat(d3.format('.0f'));
 
   /* Done setting the chart up? Time to render it!*/
+
+  console.log(chartData);
  
   d3.select(selector)          //Select the <svg> element you want to render the chart in.   
     .datum(chartData)          //Populate the <svg> element with chart data...
@@ -97,26 +106,27 @@ var createNvd3Chart = function(data, selector, param, label) {
   //Update the chart when window resizes.
   nv.utils.windowResize(function() { chart.update() });
 
+  $(selector).show(); 
   return chart;
+
   }); 
 } // End of createNvd3Chart()
 
-//-------------------NVD3 Test data generator-----------------------   
+//-------------------NVD3 data formatter-----------------------   
 function formatData(wellData, param, label) {
   console.log("Inside of getData() function");
   
   var arr = makeDataArray(wellData, param)
 
-  //Line chart data should be sent as an array of series objects.
+  //Line chart data is an array of objects.
   return[
     {
       values: arr,
       key: label,
-      color: 'blue',
-      area: true      //area - set to true if you want this line to turn into a filled area chart.
+      color: '#990000'     
     }
   ];
-}
+} // End of formatData()
 
 //-----------------------------------------------------------------
 // Create array of data
@@ -125,37 +135,26 @@ function makeDataArray(wellData, paramToChart) {
   data = []
 
   if (paramToChart == "tempOut") {
-
     data.push({x: 0, y: 0});
     for (var i = 0; i < wellData.length; i++ ) {
       data.push({x: wellData[i].temp_out, y: wellData[i].depth});
     }
-
-
   } else if (paramToChart == "tempIn") {
-
     for (var i = 0; i < wellData.length; i++ ) {
       data.push({x: wellData[i].temp_in, y: wellData[i].depth});
     }
-
   } else if (paramToChart == "pressure") {
-
     for (var i = 0; i < wellData.length; i++ ) {
       data.push({x: wellData[i].pressure, y: wellData[i].depth});
     }
-    
   } else if (paramToChart == "wob") {
-
     for (var i = 0; i < wellData.length; i++ ) {
       data.push({x: wellData[i].wob, y: wellData[i].depth});
     }
-    
   } else if (paramToChart == "rop") {
-
     for (var i = 0; i < wellData.length; i++ ) {
       data.push({x: wellData[i].rop, y: wellData[i].depth});
     }
-    
   };
 
   return data;
